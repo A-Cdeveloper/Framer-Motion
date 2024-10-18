4970ae192a34957ce1438437ed4e9346e353d101
Aleksandar <aleksandar@e-vlasotince.info>
2024-10-18 02:28:02 +0200

    ScrollAnimations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AnimationControls from "./components/AnimationControls";
 import BasicsOfMotion from "./components/BasicsOfMotion";
 import Gestures from "./components/Gestures";
+import ScrollAnimations from "./components/ScrollAnimation";
 import ViewBasedAnimations from "./components/ViewBaseAnimaions";
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
         {/* <BasicsOfMotion /> */}
         {/* <Gestures /> */}
         {/* <AnimationControls /> */}
-        <ViewBasedAnimations />
+        {/* <ViewBasedAnimations /> */}
+        <ScrollAnimations />
       </div>
     </>
   );
